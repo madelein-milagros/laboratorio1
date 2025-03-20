6198e45067b41087dc9e0485d7607744e6ce9834
madelein-milagros <ramoschamorromilagros@gmail.com>
2025-03-19 22:11:29 -0500

    terminado ejer 2 pre.py

--- 2 pre.py
+++ 2 pre.py
@@ -1,6 +1,29 @@
-# Algorithm 1: n = 1, 10, 100, 1000, 10000
+#ejercicio 02
+#correcto
 
-n_values = [1, 10, 100, 1000, 10000]
+import timeit
+import matplotlib.pyplot as plt
+
+def simple_loop(n):
+    total = 0
+    for i in range(n):
+        total += i
+    return total
+
+n_values = [10**2, 10**3, 10**4, 10**5, 10**6]
+times = []
 
 for n in n_values:
-    print(f"Current value of n: {n}")
+    start_time = timeit.default_timer()
+    simple_loop(n)
+    end_time = timeit.default_timer()
+    times.append(end_time - start_time)
+
+plt.figure(figsize=(10, 6))
+plt.plot(n_values, times, 'ro-')
+plt.xscale('log')
+plt.xlabel('n (log scale)')
+plt.ylabel('Tiempo de ejecución (s)')
+plt.title('Complejidad O(n)')
+plt.grid(True)
+plt.show()
